cf921e47574c71e796928bb9e9cfc79769dc5e42
DangHuuPhuocVinh <65935701+DangHuuPhuocVinh@users.noreply.github.com>
2020-06-26 17:01:21 +0700

    Add files via upload

---
+++ introduction web programming/Web/formLogInUp.js
@@ -0,0 +1,10 @@
+let DangNhapBTN = document.getElementById('DangNhapBtn')
+let DangKyBTN = document.getElementById('DangKyBtn')
+
+DangNhapBTN.addEventListener('click', function(){
+	window.location.replace("./dangNhap.html");
+})
+
+DangKyBTN.addEventListener('click', function(){
+	window.location.replace("./dangKy.html");
+})
